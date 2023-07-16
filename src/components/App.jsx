6be5a729355788bb925755ef513eb9d11cbfa6c9
SergeyMushtukov <hrystik90@gmail.com>
2023-07-16 12:59:36 +0300

    Закончил 1 и 2 компонент, в разработке компонент 3.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
+import { Profile } from './profile/Profile';
+import { Statistics } from './statistics/Statistics';
+import { FriendList } from './friendList/FriendList';
+import user from './user';
+import data from './data';
+import friends from './friends';
+
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload  stats" stats={data} />
+      <FriendList friends={friends} />
+    </>
   );
 };
---
+++ src/components/friendList/FriendList.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import clsx from 'clsx';
+
+const FriendListItem = ({ avatar, name, isOnline, id }) => {
+  return (
+    <li key={id} className={css.item}>
+      <span
+        className={clsx(css.status, {
+          [css.isOnline]: isOnline,
+        })}
+      ></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
+
+export const FriendList = friends => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(friend => (
+        <FriendListItem
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+          id={friend.id}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
